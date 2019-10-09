3574eb1834e5cb7865a9c7d86aa5991ab704049e
Andrés Ricaño César <Lokim0n@users.noreply.github.com>
2019-10-08 22:50:03 -0500

    Semi-final version

---
+++ php/sendmail.php
@@ -0,0 +1,10 @@
+<?php
+
+ $destino="ri259636@uaeh.edu.mx";
+ $nombre=$_POST["name"];
+ $correo=$_POST["email"];;
+ $mensaje=$_POST["message"];
+ $contenido="Nombre: ".$name."\nCorreo: ".$email."\nMensaje: "."\n".$message;
+ mail($destino,"Contacto", $contenido);
+ header("Location: ../index.html");
+?>
